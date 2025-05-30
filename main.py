be314ae618de4c8962b2dc7e18be48de0bdac9aa
Harshita <harshitarera@gmail.com>
2025-05-30 10:42:03 +0530

    text-box-frontend

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ eel.init("www")  #frontend code is present in this directory
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
-eel.start('index.html', mode=None, host='localhost', block=True)
+eel.start('index.html', mode=None, host='localhost', block=True) # eel library converts it into desktop formate
